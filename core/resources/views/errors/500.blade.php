5c955ea9dbe8ce177752ea2025c0aea1a93310ce
Abdullah Omar <62171887+AbdullahOmerDev@users.noreply.github.com>
2025-04-03 13:08:32 +0300

    1

--- core/resources/views/errors/500.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('errors.layout')
-@section('title', ___('Server Error'))
-@section('code', '500')
-@section('message', ___('Server Error'))
-@section('description', ___("Sorry for the inconvenience, but we're working on it."))
